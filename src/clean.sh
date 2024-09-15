11b988898a43f86a12738285cd3cfd897426b75d
Gary Greene <greeneg@tolharadys.net>
2024-09-15 12:26:05 -0400

    Update clean script to do more
    Signed-off-by: Gary Greene <greeneg@tolharadys.net>

--- src/clean.sh
+++ src/clean.sh
@@ -4,7 +4,12 @@ set -e
 set -u
 set -o pipefail
 
+source_dir="$(dirname "${BASH_SOURCE[0]}")"
+proj_dir="$(dirname "$(cd "${source_dir}" &> /dev/null && pwd)")"
+
+echo "Cleaning ${proj_dir}"
 umount /dev/loop0p1 ||:
 umount /dev/loop0p2 ||:
-losetup -D
-
+losetup -D ||:
+rm -fv "${proj_dir}/vdisk.img" ||:
+rm -rfv "${proj_dir}/3rdparty/busybox/build" ||:
