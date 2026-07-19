407a6820425c6ac3bec5d2ce6979742f453f7a51
Gary Greene <greeneg@tolharadys.net>
2026-07-19 13:12:46 -0400

    Update GNU Grub to 2.14
    Signed-off-by: Gary Greene <greeneg@tolharadys.net>

--- 3rdparty/grub-2.12/grub-core/osdep/efi_removable_fallback.c
+++
@@ -1,5 +0,0 @@
-#ifdef __linux__
-#include "linux/efi_removable_fallback.c"
-#else
-#include "basic/efi_removable_fallback.c"
-#endif
--- 3rdparty/grub-2.12/grub-core/osdep/journaled_fs.c
+++
@@ -1,5 +0,0 @@
-#ifdef __linux__
-#include "linux/journaled_fs.c"
-#else
-#include "basic/journaled_fs.c"
-#endif
